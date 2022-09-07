a56741381a72f34966045c2321e1b82ff7f99c00
Maxime Caucheteur <maxcauch@hotmail.com>
2022-09-07 14:10:37 +0200

    conversion of PictureCapturingListener Interface

--- app/src/main/java/com/projet/sluca/smallbrother/libs/PictureCapturingListener.kt
+++ app/src/main/java/com/projet/sluca/smallbrother/libs/PictureCapturingListener.kt
@@ -1,14 +1,13 @@
-package com.projet.sluca.smallbrother.libs;
+package com.projet.sluca.smallbrother.libs
 
-import java.util.TreeMap;
+import java.util.*
 
 /**
  * Picture capturing listener
  * @author hzitoun (zitoun.hamed@gmail.com)
+ * (written in Java by @author, converted to Kotlin by Maxime Caucheteur on 07/09/22)
  */
-
-public interface PictureCapturingListener
-{
+interface PictureCapturingListener {
     /**
      * a callback called when we've done taking a picture from a single camera
      * (use this method if you don't want to wait for ALL taken pictures to be ready @see onDoneCapturingAllPhotos)
@@ -16,13 +15,13 @@ public interface PictureCapturingListener
      * @param pictureUrl  taken picture's location on the device
      * @param pictureData taken picture's data as a byte array
      */
-    void onCaptureDone(String pictureUrl, byte[] pictureData);
+    fun onCaptureDone(pictureUrl: String?, pictureData: ByteArray?)
+
     /**
      * a callback called when we've done taking pictures from ALL AVAILABLE cameras
      * OR when NO camera was detected on the device
      *
-     * @param picturesTaken : a  Map<PictureUrl, PictureData>
+     * @param picturesTaken : a  Map<PictureUrl></PictureUrl>, PictureData>
      */
-    void onDoneCapturingAllPhotos(TreeMap<String, byte[]> picturesTaken);
-
+    fun onDoneCapturingAllPhotos(picturesTaken: TreeMap<String?, ByteArray?>?)
 }
