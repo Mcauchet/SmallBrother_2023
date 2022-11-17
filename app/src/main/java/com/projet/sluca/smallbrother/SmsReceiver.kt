6a91259e17e4e45536ff41a7b93dac92f5bc7e6f
Mcauchet <maxcauch@hotmail.com>
2022-11-17 19:39:17 +0100

    fixed SMSReceiver, still has to fix the wakeup()

--- app/src/main/java/com/projet/sluca/smallbrother/SmsReceiver.kt
+++ app/src/main/java/com/projet/sluca/smallbrother/SmsReceiver.kt
@@ -3,6 +3,7 @@ package com.projet.sluca.smallbrother
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.telephony.SmsMessage
 import android.util.Log
 import com.projet.sluca.smallbrother.activities.WorkActivity
@@ -14,73 +15,14 @@ import com.projet.sluca.smallbrother.models.UserData
  * It listens to upcoming SMS and checks if it is relevant to SmallBrother app
  * (with the [#SBxx] code)
  *
- * @author Maxime Caucheteur & Sébastien Luca (Updated on 07-11-22)
+ * @author Maxime Caucheteur & Sébastien Luca (Updated on 17-11-22)
  */
 class SmsReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context, intent: Intent) // Lors d'une réception de SMS :
-    {
-        Log.d("SMSRECEIVER", "GOT A TEXT")
-        lateinit var message: String
-        val bundle = intent.extras
-        val sms: Array<SmsMessage?>
-        userdata.loadData()
-        if (bundle != null) {
-            // Récupération du SMS reçu.
-            val pdus = bundle["pdus"] as Array<*>?
-            sms = arrayOfNulls(pdus!!.size)
-            clef = "" // Réinitialisation de la valeur de la clef.
-            for (i in sms.indices) {
-                // Tri et mémorisation du contenu du SMS.
-                sms[i] = SmsMessage.createFromPdu(pdus[i] as ByteArray, bundle.getString("format"))
-                numero = sms[i]?.originatingAddress // Set du numéro.
-                message = sms[i]!!.messageBody
-            }
-            if (message.startsWith("SmallBrother :")) // Si SMS destiné à l'appli.
-            {
-                // Isolement du code d'identification, en fin de SMS (7 caras).
-                clef = message.substring(message.length - 7)
-                val motsclef = arrayOf(
-                    "[#SB01]",  // -> réinit aidé
-                    "[#SB02]",  // -> va bien reçu par aidé
-                    "[#SB03]",  // -> oui va bien reçu par aidant
-                    "[#SB04]",  // -> urgence reçue par aidé
-                    "[#SB05]",  // -> aidé pas connecté
-                    "[#SB06]",  // -> mail d'urgence reçu
-                    "[#SB07]" // -> mode privé activé
-                )
-                if (listOf(*motsclef).contains(clef)) {
-                    if (userdata.bit == 1) // Si le Mode Privé est activé.
-                    {
-                        // Avertir :
-                        if (clef == "[#SB02]") userdata.bit =
-                            2 // cas d'un SMS
-                        else if (clef == "[#SB04]") userdata.bit =
-                            4 // cas d'un email
-                    } else {
-                        if (clef == "[#SB07]") // Récupération du temps restant si Mode Privé.
-                        {
-                            val extrait =
-                                message.substring(message.indexOf("(") + 1, message.indexOf(")"))
-                            tempsrestant = extrait
-                        }
-
-                        // lancement de la "WorkActivity".
-                        val intnt = Intent(context, WorkActivity::class.java)
-                        intnt.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        context.startActivity(intnt)
-                    }
-                }
-            }
-        }
-    }
-
-
-/*
     override fun onReceive(context: Context?, intent: Intent?) {
         val bundle = intent?.extras
         Log.d("ONRCV SMS", bundle.toString())
-        userdata.loadData()
+        userdata?.loadData()
         val message = getTextFromSms(bundle)
         Log.d("MSG SMSRCV", message)
         clef = ""
@@ -105,11 +47,11 @@ class SmsReceiver : BroadcastReceiver() {
         if (!listOf(*motsclef).contains(clef)) return
 
         //La clef est dans la liste
-        if (userdata.bit == 1) // Si le Mode Privé est activé.
+        if (userdata?.bit == 1) // Si le Mode Privé est activé.
         {
             // Avertir :
-            if (clef == "[#SB02]") userdata.bit = 2 // cas d'un SMS
-            else if (clef == "[#SB04]") userdata.bit = 4 // cas d'un email
+            if (clef == "[#SB02]") userdata?.bit = 2 // cas d'un SMS
+            else if (clef == "[#SB04]") userdata?.bit = 4 // cas d'un email
         } else {
             if (clef == "[#SB07]") // Récupération du temps restant si Mode Privé.
             {
@@ -156,7 +98,7 @@ class SmsReceiver : BroadcastReceiver() {
      */
     private fun getSmsMsg(pdu: ByteArray?, format: String?): SmsMessage? {
         return SmsMessage.createFromPdu(pdu, format)
-    }*/
+    }
 
     companion object {
         var numero: String? = null // Retiendra le numéro de l'envoyeur.
@@ -164,58 +106,6 @@ class SmsReceiver : BroadcastReceiver() {
 
         lateinit var tempsrestant: String // Retiendra le temps restant de Mode Privé pour l'Aidant.
 
-        lateinit var userdata: UserData// Liaison avec les données globales de l'utilisateur.
-
-
-        // Placement des données dans un array, séparation par le retour-charriot.
-        // Suppression du fichier de données s'il existe déjà (pour éviter concaténation).
-        // Ecriture.
-        // Rapatriement des données :
-        // Récupération du contenu du fichier :
-
-        // ---- Méchanismes de communication AideActivity / SmsReceiver.
-        /*var bit: Int?
-            get() {
-                userdata.loadData()
-                Log.d("USERDATA SMSRCV", userdata.toString())
-                val data = File(userdata.path + "/SmallBrother/bit.txt")
-                if (data.exists()) {
-                    try  // Récupération du contenu du fichier :
-                    {
-                        // Placement des données dans un array, séparation par le retour-charriot.
-                        val br =
-                            BufferedReader(FileReader(data))
-                        val dataLine = IOUtils.toString(br)
-                        val dataTab =
-                            dataLine.split("\r").toTypedArray()
-
-                        // Rapatriement des données :
-                        return Integer.valueOf(dataTab[0])
-                    } catch (e: FileNotFoundException) {
-                        e.printStackTrace()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                }
-                return 0
-            }
-            set(bit) {
-                userdata.loadData()
-                Log.d("SETBIT UD", userdata.toString())
-                try {
-                    val bitFile = File(userdata.path + "/SmallBrother/bit.txt")
-                    if (!bitFile.exists()) bitFile.createNewFile() else {
-                        // Suppression du fichier de données s'il existe déjà (pour éviter concaténation).
-                        val previousFile = File(userdata.path + "/SmallBrother/bit.txt")
-                        previousFile.delete()
-                    }
-
-                    // Ecriture.
-                    val writer = BufferedWriter(FileWriter(bitFile, true))
-                    writer.write(bit.toString())
-                    writer.close()
-                } catch (_: IOException) {
-                }
-            }*/
+        var userdata: UserData? = null// Liaison avec les données globales de l'utilisateur.
     }
 }
