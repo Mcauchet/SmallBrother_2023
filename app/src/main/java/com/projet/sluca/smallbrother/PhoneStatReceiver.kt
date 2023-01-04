8b0370427a5774dbfda12aefb697facf72e7d5bf
Mcauchet <maxcauch@hotmail.com>
2023-01-04 23:55:13 +0100

    refactor code to fit 'short units of code' PhoneStatReceiver

--- app/src/main/java/com/projet/sluca/smallbrother/PhoneStatReceiver.kt
+++ app/src/main/java/com/projet/sluca/smallbrother/PhoneStatReceiver.kt
@@ -9,9 +9,9 @@ import com.projet.sluca.smallbrother.activities.WorkActivity
 import com.projet.sluca.smallbrother.models.UserData
 
 /***
- * PhoneStatReceiver manages information when a phone call is made between aidant and aide
+ * PhoneStatReceiver manages information when a phone call is received on the user's phone
  *
- * @author Sébastien Luca & Maxime Caucheteur (Updated on 14-12-22)
+ * @author Sébastien Luca & Maxime Caucheteur (Updated on 04-01-2023)
  */
 class PhoneStatReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -23,34 +23,36 @@ class PhoneStatReceiver : BroadcastReceiver() {
         Log.i(TAG, "call OUT:"+phoneNumber);
         */
         } else {
-            val tm = context.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
-            //TODO see deprecated
-            if (tm.callState == TelephonyManager.CALL_STATE_RINGING) {
-                // Set du numéro de l'appelant.
-                if(intent.hasExtra("incoming_number")) {
-                    callNumber = intent.getStringExtra("incoming_number").toString()
-                }
-                if (userData.bit == 1) // Si le Mode Privé est activé.
-                {
-                    // Avertir :
-                    userData.bit = 3 // cas d'un appel
-                } else {
-                    // Déclaration d'un passage dans la WorkActivity pour éviter que, au retour dans
-                    // AideActivity, ne soit généré un doublon du Handler local.
-                    userData.esquive = true
+            processCall(context, intent)
+        }
+    }
 
-                    // lancement de la "WorkActivity".
-                    val intnt = Intent(context, WorkActivity::class.java)
-                    intnt.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    context.startActivity(intnt)
-                }
+    /**
+     * Process the phone call
+     * @param [context] the context of the activity
+     * @param [intent] the intent on receive of a call
+     * @author Maxime Caucheteur
+     * @version 1.2 (Updated on 04-01-2023)
+     */
+    private fun processCall(context: Context, intent: Intent) {
+        val tm = context.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
+        if (tm.callState == TelephonyManager.CALL_STATE_RINGING) {
+            if(intent.hasExtra("incoming_number")) callNumber = intent
+                .getStringExtra("incoming_number").toString()
+            if (userData.bit == 1) userData.bit = 3 // Private mode ON
+            else {
+                // Avoids creating a second Handler
+                userData.esquive = true
+                val intnt = Intent(context, WorkActivity::class.java)
+                intnt.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(intnt)
             }
         }
     }
 
     companion object {
         var callNumber = ""
-        lateinit var userData: UserData // Liaison avec les données globales de l'utilisateur.
+        lateinit var userData: UserData 
 
         fun catchCallNumber(): String = callNumber
 
