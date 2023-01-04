7da9038f773659812ad23b9a56876d30be8f9a66
Mcauchet <maxcauch@hotmail.com>
2023-01-04 23:17:17 +0100

    refactor code to fit 'short units of code' InstallDantPicActivity

--- app/src/main/java/com/projet/sluca/smallbrother/activities/InstallDantPicActivity.kt
+++ app/src/main/java/com/projet/sluca/smallbrother/activities/InstallDantPicActivity.kt
@@ -23,39 +23,28 @@ import java.util.*
  * class InstallDantPicActivity manages the capture of the aidé picture
  *
  * @author Maxime Caucheteur (with contribution of Sébatien Luca (Java version))
- * @version 1.2 (updated on 27-12-2022)
+ * @version 1.2 (updated on 04-01-2023)
  */
 class InstallDantPicActivity : AppCompatActivity() {
 
-    var vibreur = Vibration() // Instanciation d'un vibreur.
-    lateinit var userData: UserData // Liaison avec les données globales de l'utilisateur.
+    var vibreur = Vibration()
+    lateinit var userData: UserData
     private lateinit var apercu : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Etablissement de la liaison avec la vue res/layout/activity_installdantpic.xml.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_installdantpic)
 
-        // Etablissement de la liaison avec la classe UserData.
         userData = application as UserData
 
         val btnCapture: Button = findViewById(R.id.btn_capture)
         val btnBack: Button = findViewById(R.id.btn_previous)
         val btnEnd: Button = findViewById(R.id.btn_terminer)
 
-        // Identification de l'aperçu.
-        apercu = findViewById(R.id.apercu)
-
-        // Par défaut : afficher la photo enregistrée, s'il y en a une.
-        val path = userData.path + "/SmallBrother/photo_aide.jpg"
-        Log.d("path picture", path)
-        val file = File(path)
-        if (file.exists()) apercu.setImageURI(Uri.fromFile(file))
+        showPicture()
 
         btnCapture.setOnClickListener {
             vibreur.vibration(this, 100)
-
-            // Lancement de l'activité de capture.
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             intent.putExtra("requestCode", 7)
             startActivityForResult(intent, 7)
@@ -68,37 +57,41 @@ class InstallDantPicActivity : AppCompatActivity() {
 
         btnEnd.setOnClickListener {
             vibreur.vibration(this, 100)
-            // Rétablissement des boutons retour, au cas où désactivé par ReglagesActivity.
             userData.canGoBack = true
-            // Transition vers l'activity suivante.
             val intent = Intent(this, QRCodeInstallActivity::class.java)
             startActivity(intent)
         }
-
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
-    // --> Au retour à la présente actvité, si une photo a été prise :
+    /**
+     * Shows the Aide's picture if it exists
+     * @author Maxime Caucheteur
+     * @version 1.2 (Updated on 04-01-2023)
+     */
+    private fun showPicture() {
+        apercu = findViewById(R.id.apercu)
+        val path = userData.path + "/SmallBrother/photo_aide.jpg"
+        val file = File(path)
+        if (file.exists()) apercu.setImageURI(Uri.fromFile(file))
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         Log.d("result code", resultCode.toString())
         if (requestCode == 7 && resultCode == RESULT_OK) {
-            val bitmap = data!!.extras!!["data"] as Bitmap? // Récupération de la photo
-
-            // -> Sauvegarde de la photo.
-            val image =
-                userData.path + "/SmallBrother/photo_aide.jpg" // chemin de fichier globalisé.
+            val bitmap = data!!.extras!!["data"] as Bitmap?
+            val image = userData.path + "/SmallBrother/photo_aide.jpg"
             try {
                 bitmap!!.compress(CompressFormat.JPEG, 100, FileOutputStream(image))
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
             }
-            apercu.setImageBitmap(bitmap) // Affichage de la photo dans l'ImageView "aperçu".
+            apercu.setImageBitmap(bitmap)
         }
     }
 
-    // --> Par sécurité : retrait du retour en arrière dans cette activity.
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             moveTaskToBack(false)
