3804e001aa6a71974999f073fd64c6fc61f90441
Mcauchet <maxcauch@hotmail.com>
2023-04-04 13:43:13 +0200

    documentation of UserDataManager

--- app/src/main/java/com/projet/sluca/smallbrother/UserDataManager.kt
+++ app/src/main/java/com/projet/sluca/smallbrother/UserDataManager.kt
@@ -3,10 +3,22 @@ package com.projet.sluca.smallbrother
 import android.app.Application
 import com.projet.sluca.smallbrother.models.UserData
 
+/**
+ * UserDataManager acts as a Singleton for the userData class invoked in activities
+ * @author Maxime Caucheteur
+ * @version 1.2 (Updated on 04-04-2023)
+ */
 object UserDataManager {
 
     private var userData: UserData? = null
 
+    /**
+     * Returns the userData object
+     * @param application
+     * @return userData
+     * @author Maxime Caucheteur
+     * @version 1.2 (Updated on 04-04-2023)
+     */
     fun getUserData(application: Application): UserData {
         if(userData == null) {
             userData = application as UserData
